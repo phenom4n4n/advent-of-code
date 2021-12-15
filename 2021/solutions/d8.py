4c255f9be69aaab9b8cb795089e86a43c6db77de
PhenoM4n4n <phenom4n4n@gmail.com>
2021-12-15 10:48:25 -0800

    cleanup 2021/d8

--- 2021/solutions/d8.py
+++ 2021/solutions/d8.py
@@ -1,5 +1,4 @@
-from itertools import permutations
-from typing import Dict, List, FrozenSet, Tuple
+from typing import Dict, List, FrozenSet
 
 from utils import run
 
@@ -9,30 +8,7 @@ len_to_num = {
     3: 7,
     7: 8,
 }
-num_to_seg = {
-    0: "abcefg",
-    1: "cf",
-    2: "acdeg",
-    3: "acdfg",
-    4: "bcdf",
-    5: "abdfg",
-    6: "abdefg",
-    7: "acf",
-    8: "abcdefg",
-    9: "abcdfg",
-}
 Key = Dict[FrozenSet[str], int]
-
-test = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
 
 
 @run("\n")
@@ -50,7 +26,6 @@ def decode_line(line: str) -> Key:
     i, _ = line.split(" | ", 2)
     key = {}
     reverse_key = {}
-    seg_key = {}
 
     for sig in i.split():
         if n := len_to_num.get(len(sig)):
